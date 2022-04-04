98aa02710cce5f682c3d76b38eb5269232c3d5db
Camillaatek <camillaatek@gmail.com>
2022-04-04 23:12:15 +0200

    feat: added media query on login page

--- holidaze-exam/src/components/pages/Login.jsx
+++ holidaze-exam/src/components/pages/Login.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import LoginForm from '../LoginForm'
+import undraw from '../../images/undraw.png'
 
 const Login = () => {
   return (
-    <div className='login'>
-        <h1>Welcome to Holidaze</h1>
-        <p>Log in to see bookings, messages and to edit enquiries.</p>
-        <LoginForm />
-    </div>
-  )
+         <>
+            <div className='login'>
+                <h1>Welcome to Holidaze</h1>
+                <p>Log in to see bookings, messages and to edit enquiries.</p>
+                <LoginForm />
+                <p>Forgot your Password? Click here.</p>
+             
+            </div>
+      </>
+    )
 }
 
 export default Login
