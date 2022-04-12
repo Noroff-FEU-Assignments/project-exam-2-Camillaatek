67686920284c569f947b7d1b31ee2f197cd63417
Camillaatek <camillaatek@gmail.com>
2022-04-13 00:26:40 +0200

    feat: added hooks

--- holidaze-exam/src/components/pages/admin/pages/AllHotels.jsx
+++ holidaze-exam/src/components/pages/admin/pages/AllHotels.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import AuthContext from '../../../../context/AuthContext'
+import useToggle from '../../../../hooks/useToggle'
+import { BOOKINGS_PATH } from '../../../../utils/Api'
 import Dashboard from '../components/Dashboard'
 
 const AllHotels = () => {
+  const [isTriggered, setIsTriggered] = useToggle()
+  const [bookings, setBookings] = useState([])
+  const [auth] = useContext(AuthContext)
+  const http = useAxios()
+
+  useEffect(() => {
+    
+    const fetchData = async () => {
+    const data = await http.get(BOOKINGS_PATH)
+    setBookings(data.data.data)
+
+  }
+
+  fetchData().catch((error) => setError(error.response.data.error))
+  }, [isTriggered, auth])
   return (
-      <>
-      <div className="hotels">
-      <Dashboard />
-    <div>AllHotels</div>
+      <div className="hotel">
+        <div className="hotel__dash">
+          <Dashboard />
+        </div>
+        <div className="hotel__page">
+
+        </div>
     </div>
-    </>
   )
 }
 
