548bd78907afad696b6b34d360c8dda79b51f670
Camillaatek <camillaatek@gmail.com>
2022-04-25 00:25:54 +0200

    feat: styled id page

--- holidaze-exam/src/components/pages/Details.jsx
+++ holidaze-exam/src/components/pages/Details.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import useAxios from "../../hooks/useAxios"
 import useToggle from "../../hooks/useToggle"
 import { BOOKINGS_PATH } from "../../utils/Api"
+import StarIcon from '@mui/icons-material/Star';
 
 
 
@@ -23,11 +24,25 @@ const Details = () => {
   
 
   return (
-    <div className="detail__page">
-    <img src={details.image_url} alt="" />
-      <h1>{details.name}</h1>
-      <h2>{details.description}</h2>
-      <p>{details.cost}</p>
+    <div className="details">
+        <img src={details.image_url} alt="" />
+        <div className="details__title">
+            <h1>{details.name}</h1>
+            <p>{details.cost}Kr / Night</p>
+        </div>
+        <div className="details__info">
+            <p>{details.location}</p>
+            <p><StarIcon style={{ fontSize: 15, color: '#FFA800' }}/>{details.rating}</p>
+        </div>
+        <div className="details__desc">
+            <p>{details.description}</p>
+        </div>
+        <Link to={`/`}>
+                    <button className='details__button'>
+                      Book Now
+                      </button>
+                  </Link>
+        
     </div>
   )
 }
